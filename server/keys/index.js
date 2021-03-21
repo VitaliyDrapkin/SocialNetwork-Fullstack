c01dcc2e6c74ee613837d1284e1ed606ded62e65
Vitaliy <drapkinvitaliy@gmail.com>
2021-03-21 15:11:18 +0200

    keys

--- server/keys/index.js
+++ server/keys/index.js
@@ -1,5 +1,1 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./key.prod");
-} else {
-  module.exports = require("./key.dev");
-}
+module.exports = require("./key.git");
