a90368f3356a7a908a29ebf8514bc446ef2485c5
Vitaliy <drapkinvitaliy@gmail.com>
2021-03-21 15:21:18 +0200

    a

---
+++ server/keys/key.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB_URL:
+      "mongodb+srv://Vitaliy:CAUpaYujDJii5Gca@cluster0.wbgd5.mongodb.net/Social_Network?retryWrites=true&w=majority",
+    BASE_URL: "http://localhost:3001/",
+  };
+  
